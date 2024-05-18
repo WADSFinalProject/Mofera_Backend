aefafcb92a705be5c497c12e6f16d3066164378b
I Kadek Gita Pradnya Widagda <skyb3105@gmail.com>
2024-05-18 13:43:46 +0700

    CRUD date query

--- backend/crud.py
+++ backend/crud.py
@@ -1,12 +1,15 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import and_, or_
 import models
 import schemas
+from datetime import date
 
 def get_wet_leaves_by_id(db: Session, wet_leaves_id: int):
     return db.query(models.Collection).filter(models.Collection.id == wet_leaves_id).first()
 
 def get_dry_leaves_by_id(db: Session, dry_leaves_id: int):
     return db.query(models.DryLeaves).filter(models.DryLeaves.id == dry_leaves_id).first()
+
 def get_flour_by_id(db: Session, flour_id: int):
     return db.query(models.Flour).filter(models.Flour.id == flour_id).first()
 
@@ -16,81 +19,137 @@ def get_shipping_by_id(db: Session, shipping_id: int):
 def get_checkpoint_by_id(db: Session, checkpoint_id: int):
     return db.query(models.CheckpointData).filter(models.CheckpointData.id == checkpoint_id).first()
 
-def get_centra_notifications_id(db: Session, centra_notif_id: int):
+def get_centra_notifications_by_id(db: Session, centra_notif_id: int):
     return db.query(models.CentraNotification).filter(models.CentraNotification.id == centra_notif_id).first()
 
 def get_reception_packages_by_id(db: Session, reception_packages_id: int):
     return db.query(models.ReceptionPackage).filter(models.ReceptionPackage.id == reception_packages_id).first()
 
-def get_checkpoints(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.CheckpointData).offset(skip).limit(limit).all()
+def get_checkpoints(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.CheckpointData)
+    if date_filter:
+        if before:
+            query = query.filter(models.CheckpointData.arrival_date < date_filter)
+        elif after:
+            query = query.filter(models.CheckpointData.arrival_date > date_filter)
+        else:
+            query = query.filter(models.CheckpointData.arrival_date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_wet_leaves(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Collection).offset(skip).limit(limit).all()
+def get_wet_leaves(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.Collection)
+    if date_filter:
+        if before:
+            query = query.filter(models.Collection.retrieval_date < date_filter)
+        elif after:
+            query = query.filter(models.Collection.retrieval_date > date_filter)
+        else:
+            query = query.filter(models.Collection.retrieval_date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_dry_leaves(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Dry).offset(skip).limit(limit).all()
+def get_dry_leaves(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.DryLeaves)
+    if date_filter:
+        if before:
+            query = query.filter(models.DryLeaves.exp_date < date_filter)
+        elif after:
+            query = query.filter(models.DryLeaves.exp_date > date_filter)
+        else:
+            query = query.filter(models.DryLeaves.exp_date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_flour(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Flour).offset(skip).limit(limit).all()
+def get_flour(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.Flour)
+    if date_filter:
+        if before:
+            query = query.filter(models.Flour.finish_time < date_filter)
+        elif after:
+            query = query.filter(models.Flour.finish_time > date_filter)
+        else:
+            query = query.filter(models.Flour.finish_time == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_shipping(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.Shipping).offset(skip).limit(limit).all()
+def get_shipping(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.Shipping)
+    if date_filter:
+        if before:
+            query = query.filter(models.Shipping.departure_date < date_filter)
+        elif after:
+            query = query.filter(models.Shipping.departure_date > date_filter)
+        else:
+            query = query.filter(models.Shipping.departure_date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_centra_notifications(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.CentraNotification).offset(skip).limit(limit).all()
+def get_centra_notifications(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.CentraNotification)
+    if date_filter:
+        if before:
+            query = query.filter(models.CentraNotification.date < date_filter)
+        elif after:
+            query = query.filter(models.CentraNotification.date > date_filter)
+        else:
+            query = query.filter(models.CentraNotification.date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def get_reception_packages(db: Session, skip: int = 0, limit: int = 10):
-    return db.query(models.ReceptionPackage).offset(skip).limit(limit).all()
+def get_reception_packages(db: Session, skip: int = 0, limit: int = 10, date_filter: date = None, before: bool = None, after: bool = None):
+    query = db.query(models.ReceptionPackage)
+    if date_filter:
+        if before:
+            query = query.filter(models.ReceptionPackage.receival_date < date_filter)
+        elif after:
+            query = query.filter(models.ReceptionPackage.receival_date > date_filter)
+        else:
+            query = query.filter(models.ReceptionPackage.receival_date == date_filter)
+    return query.offset(skip).limit(limit).all()
 
-def create_wet_leaves (db: Session, wet_leaves: schemas.WetLeavesRecord):
+def create_wet_leaves(db: Session, wet_leaves: schemas.WetLeavesRecord):
     db_wet_leaves = models.Collection(retrieval_date=wet_leaves.retrieval_date, weight=wet_leaves.weight)
     db.add(db_wet_leaves)
     db.commit()
     db.refresh(db_wet_leaves)
     return db_wet_leaves
 
-def create_dry_leaves (db: Session, dry_leaves: schemas.DryLeavesRecord):
-    db_dry_leaves = models.Dry(exp_date=dry_leaves.exp_date, weight=dry_leaves.weight)
+def create_dry_leaves(db: Session, dry_leaves: schemas.DryLeavesRecord):
+    db_dry_leaves = models.DryLeaves(exp_date=dry_leaves.exp_date, weight=dry_leaves.weight)
     db.add(db_dry_leaves)
     db.commit()
     db.refresh(db_dry_leaves)
     return db_dry_leaves
 
-def create_flour (db: Session, flour: schemas.FlourRecord):
+def create_flour(db: Session, flour: schemas.FlourRecord):
     db_flour = models.Flour(finish_time=flour.finish_time, weight=flour.weight)
     db.add(db_flour)
     db.commit()
     db.refresh(db_flour)
     return db_flour
 
-def create_shipping (db: Session, shipping: schemas.ShippingDepature):
+def create_shipping(db: Session, shipping: schemas.ShippingDepature):
     db_shipping = models.Shipping(departure_date=shipping.departure_date, expedition_id=shipping.expedition_id)
     db.add(db_shipping)
     db.commit()
     db.refresh(db_shipping)
     return db_shipping
 
-def create_checkpoint (db: Session, checkpoint: schemas.CheckpointDataRecord):
+def create_checkpoint(db: Session, checkpoint: schemas.CheckpointDataRecord):
     db_checkpoint = models.CheckpointData(**checkpoint.dict())
     db.add(db_checkpoint)
     db.commit()
     db.refresh(db_checkpoint)
     return db_checkpoint
 
-# def create_centra_notifications (db: Session, centra_notif: schemas.CentraNotification):
-#     db_centra_notif = models.CentraNotification(message=centra_notif.message, user_id=centra_notif.user_id)
-#     db.add(db_centra_notif)
-#     db.commit()
-#     db.refresh(db_centra_notif)
-#     return db_centra_notif
+def create_centra_notifications(db: Session, centra_notif: schemas.CentraNotification):
+    db_centra_notif = models.CentraNotification(message=centra_notif.message, user_id=centra_notif.user_id)
+    db.add(db_centra_notif)
+    db.commit()
+    db.refresh(db_centra_notif)
+    return db_centra_notif
 
-# def create_reception_packages (db: Session, reception_packages: schemas.ReceptionPackage):
-#     db_reception_packages = models.ReceptionPackage(**reception_packages.dict())
-#     db.add(db_reception_packages)
-#     db.commit()
-#     db.refresh(db_reception_packages)
-#     return db_reception_packages
+def create_reception_packages(db: Session, reception_packages: schemas.ReceptionPackage):
+    db_reception_packages = models.ReceptionPackage(**reception_packages.dict())
+    db.add(db_reception_packages)
+    db.commit()
+    db.refresh(db_reception_packages)
+    return db_reception_packages
 
 def update_wet_leaves(db: Session, wet_leaves_id: int, wet_leaves: schemas.WetLeavesBase):
     db_wet_leaves = get_wet_leaves_by_id(db, wet_leaves_id)
@@ -137,23 +196,23 @@ def update_checkpoint(db: Session, checkpoint_id: int, checkpoint: schemas.Check
         db.refresh(db_checkpoint)
     return db_checkpoint
 
-# def update_centra_notifications(db: Session, centra_notif_id: int, centra_notif: schemas.CentraNotification):
-#     db_centra_notif = get_centra_notifications_id(db, centra_notif_id)
-#     if db_centra_notif:
-#         for key, value in centra_notif.dict().items():
-#             setattr(db_centra_notif, key, value)
-#         db.commit()
-#         db.refresh(db_centra_notif)
-#     return db_centra_notif
+def update_centra_notifications(db: Session, centra_notif_id: int, centra_notif: schemas.CentraNotification):
+    db_centra_notif = get_centra_notifications_by_id(db, centra_notif_id)
+    if db_centra_notif:
+        for key, value in centra_notif.dict().items():
+            setattr(db_centra_notif, key, value)
+        db.commit()
+        db.refresh(db_centra_notif)
+    return db_centra_notif
 
-# def update_reception_packages(db: Session, reception_packages_id: int, reception_packages: schemas.ReceptionPackage):
-#     db_reception_packages = get_reception_packages_by_id(db, reception_packages_id)
-#     if db_reception_packages:
-#         for key, value in reception_packages.dict().items():
-#             setattr(db_reception_packages, key, value)
-#         db.commit()
-#         db.refresh(db_reception_packages)
-#     return db_reception_packages
+def update_reception_packages(db: Session, reception_packages_id: int, reception_packages: schemas.ReceptionPackage):
+    db_reception_packages = get_reception_packages_by_id(db, reception_packages_id)
+    if db_reception_packages:
+        for key, value in reception_packages.dict().items():
+            setattr(db_reception_packages, key, value)
+        db.commit()
+        db.refresh(db_reception_packages)
+    return db_reception_packages
 
 def delete_wet_leaves(db: Session, wet_leaves_id: int):
     db_wet_leaves = get_wet_leaves_by_id(db, wet_leaves_id)
@@ -191,7 +250,7 @@ def delete_checkpoint(db: Session, checkpoint_id: int):
     return db_checkpoint
 
 def delete_centra_notifications(db: Session, centra_notif_id: int):
-    db_centra_notif = get_centra_notifications_id(db, centra_notif_id)
+    db_centra_notif = get_centra_notifications_by_id(db, centra_notif_id)
     if db_centra_notif:
         db.delete(db_centra_notif)
         db.commit()
@@ -203,4 +262,3 @@ def delete_reception_packages(db: Session, reception_packages_id: int):
         db.delete(db_reception_packages)
         db.commit()
     return db_reception_packages
-
